09912929bb221ca27dc6c0dce5afb6def923233b
Jonathan Yellin <jyellin@synamedia.com>
2019-04-30 21:10:18 +0300

    fields missing from generated csv file

--- relation_extraction_utils/generate_csv_file.py
+++ relation_extraction_utils/generate_csv_file.py
@@ -93,7 +93,13 @@ def generate_csv_file(train_data_path, relation_name, csv_file_path, batch_size=
 
         index_lookup_str = '\n'.join(['{0}: \'{1}\''.format(tuple[0], tuple[1]) for tuple in texts])
 
-        csv_out.writerow([count, sentence, obj, subj, dependencies, index_lookup_str, lemmas])
+        csv_out.writerow([count, sentence, obj, subj,
+                          dependencies, index_lookup_str, lemmas,
+                          None, None,
+                          subj_start, subj_end,
+                          obj_start, obj_end,
+                          None
+                          ])
 
 
 if __name__ == "__main__":
