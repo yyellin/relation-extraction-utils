3171482a42a73fe53895cdff399da2b9eba0a200
Jonathan Yellin <jyellin@synamedia.com>
2019-04-30 20:11:55 +0300

    fix colab path and setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='relation_extraction_utils',
-    version='',
-    packages=['relation_extraction_utils'],
+    version='0.0.2',
+    packages=['relation_extraction_utils', 'relation_extraction_utils.internal'],
     url='https://github.com/comp-aspects-of-appl-linguistics/relation_extraction_utils',
     license='GPLv3',
     author='Or, Shachar, Jonathan',
