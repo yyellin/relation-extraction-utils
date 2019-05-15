aa51217d5982ebe7a0ddba80623cb421343263f1
Jonathan Yellin <jyellin@synamedia.com>
2019-05-15 10:16:35 +0300

    silly bug - requires more testing

--- relation_extraction_utils/internal/sync_tags.py
+++ relation_extraction_utils/internal/sync_tags.py
@@ -21,7 +21,7 @@ class SyncTags(object):
         while len(list_b_lookup) > 0 and index_a <= len(list_a) and index_b <= len(list_b):
 
             # Possibility 1: pointing to same token
-            if list_a[index_a] == list_b[index_b]:
+            if list_a[index_a - 1] == list_b[index_b - 1]:
                 if index_b in list_b_lookup.keys():
                     list_a_lookup[index_a] = list_b_lookup[index_b]
                     del list_b_lookup[index_b]
@@ -36,8 +36,8 @@ class SyncTags(object):
             #       will get that lookup
 
             a_is_substring = False
-            word_b = list_b[index_b]
-            while list_a[index_a] in word_b:
+            word_b = list_b[index_b - 1]
+            while list_a[index_a - 1] in word_b:
                 a_is_substring = True
 
                 if index_b in list_b_lookup.keys():
@@ -63,8 +63,8 @@ class SyncTags(object):
             #       lost)
 
             b_is_substring = False
-            word_a = list_a[index_b]
-            while list_b[index_b] in word_a:
+            word_a = list_a[index_b - 1]
+            while list_b[index_b - 1] in word_a:
                 b_is_substring = True
 
                 if index_b in list_b_lookup.keys():
@@ -85,7 +85,7 @@ class SyncTags(object):
                 tmp = index_a
                 found = False
                 while tmp < len(list_a):
-                    if list_a[tmp] == list_b[index_b]:
+                    if list_a[tmp - 1] == list_b[index_b - 1]:
                         found = True
                         break
                     tmp += 1
